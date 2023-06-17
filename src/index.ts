5776d46298ab67678e4cc20ac142e658a23af81d
pvwnthem <pvwn.them@gmail.com>
2023-06-16 23:02:05 -0400

    set custom headers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import cors from "cors"
 import { createProxyMiddleware } from "http-proxy-middleware"
 
@@ -10,6 +10,14 @@ const port = 8888;
 const app: Express = express();
 
 // app configuration
+function customHeaders( req: Request, res: Response, next: NextFunction ){
+  
+    res.setHeader( 'X-Powered-By', 'MinerInsight' );
+  
+    next();
+}
+
+app.use(customHeaders)
 
 app.use(cors({
     origin: '*'
