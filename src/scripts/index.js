ed51ed25ee6d1b546ef4aaf49a59c23fdf69550c
pvwnthem <pvwn.them@gmail.com>
2023-06-17 01:37:41 -0400

    add form function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,6 +12,44 @@ function main() {
     closeModal.addEventListener("click", () => {
         modal.close();
     });
+
+    var form = document.getElementById('addform');
+
+    // Add a submit event listener to the form
+    form.addEventListener('submit', handleFormSubmission);
+    
+}
+
+function handleFormSubmission(event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    // Get the form element
+    var form = event.target;
+
+    // Get the form data
+    var formData = new FormData(form);
+
+    // Create a new XMLHttpRequest object
+    var xhr = new XMLHttpRequest();
+
+    // Configure the request
+    xhr.open(form.method, form.action, true);
+
+    // Set the callback function when the request completes
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        var response = xhr.responseText;
+        // Handle the response here
+        console.log(response);
+      } else {
+        // Handle the error case
+        console.error('Request failed. Status: ' + xhr.status);
+      }
+    };
+
+    // Send the request
+    xhr.send(formData);
+
 }
 
 window.onload = main
