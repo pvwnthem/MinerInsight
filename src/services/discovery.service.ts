3815dcf4651b87ef8a8040865b7575fbfb1dc4e9
pvwnthem <pvwn.them@gmail.com>
2023-06-16 22:23:02 -0400

    add paths, axios, start service

---
+++ src/services/discovery.service.ts
@@ -0,0 +1,17 @@
+import { miners } from "@/constants/miners";
+import { type miner } from "@/types/miner.types";
+import axios from 'axios'
+
+export const discoverRunning = async (): Promise<miner[]> => 
+{
+    let out: miner[] = [];
+
+    for (const miner of miners) 
+    {
+        const response = await axios.get(miner.baseUrl)
+        if (response) {
+            out.push(miner)
+        }
+    }
+    return out;
+}
